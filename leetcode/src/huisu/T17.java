8bc8f8d475a6ec07e40d24d9f82858733b843f80
li666 <lichenhao6669@gmail.com>
2026-03-12 20:50:34 +0800

    5 commit

--- leetcode/src/huisu/T17.java
+++ leetcode/src/huisu/T17.java
@@ -4,6 +4,10 @@ package huisu;
 //https://leetcode-cn.com/problems/permutations/
 //回溯
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
 
 
@@ -22,4 +26,29 @@ package huisu;
 输出：[[1]]*/
 
 public class T17 {
+    public List<List<Integer>> permute(int[] nums) {
+         List<List<Integer>> res = new ArrayList<>();
+         List<Integer> path = new ArrayList<>();
+        for (int num : nums) {
+            path.add(num);
+        }
+        int n = nums.length;
+        backtrack(n,res, path,0);
+        return res;
+    }
+    private void backtrack(int n, List<List<Integer>> res, List<Integer> path, int index) {
+        if (index == n) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = index; i < n; i++) {
+            //做选择
+            Collections.swap(path, index, i);
+            //递归
+            backtrack(n, res, path, index + 1);
+            //撤销选择
+            Collections.swap(path, index, i);
+        }
+    }
 }
+
--- leetcode/src/zichuan/T13.java
+++ leetcode/src/zichuan/T13.java
@@ -4,6 +4,9 @@ package zichuan;
 //https://leetcode.cn/problems/subarray-sum-equals-k/
 //子串
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
 子数组是数组中元素的连续非空序列。
 
@@ -18,6 +21,25 @@ package zichuan;
 
 public class T13 {
     public int subarraySum(int[] nums, int k) {
-           666
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        // map 用于存储：前缀和 -> 该前缀和出现的次数
+        Map<Integer, Integer> map = new HashMap<>();
+        // 初始化，前缀和为 0 出现 1 次（处理从索引 0 开始的子数组）
+        map.put(0, 1);
+        int count = 0;
+        int currentSum = 0;
+        for (int num : nums) {
+            // 更新当前前缀和
+            currentSum += num;
+            // 如果 (currentSum - k) 在 map 中存在，说明找到了满足条件的子数组
+            if (map.containsKey(currentSum - k)) {
+                count += map.get(currentSum - k);
+            }
+            // 将当前前缀和存入 map，次数 +1
+            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
+        }
+        return count;
     }
 }
