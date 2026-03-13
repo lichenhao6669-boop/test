dbe724ce38922d6c37022ef79b15ad52eef083fe
li666 <lichenhao6669@gmail.com>
2026-03-13 21:06:01 +0800

    6 commit

--- leetcode/src/tulun/T9.java
+++ leetcode/src/tulun/T9.java
@@ -28,6 +28,30 @@ package tulun;
 
 public class T9 {
     public int numIslands(char[][] grid) {
-       666
+        if (grid == null || grid.length == 0){
+            return 0;
+        }
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 }
--- leetcode/src/zichuan/T13.java
+++ leetcode/src/zichuan/T13.java
@@ -21,6 +21,7 @@ import java.util.Map;
 
 public class T13 {
     public int subarraySum(int[] nums, int k) {
+        // 判断数组是否为空或长度为0，如果是则直接返回0
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -28,18 +29,24 @@ public class T13 {
         Map<Integer, Integer> map = new HashMap<>();
         // 初始化，前缀和为 0 出现 1 次（处理从索引 0 开始的子数组）
         map.put(0, 1);
+        // count 用于记录满足条件的子数组个数
         int count = 0;
+        // currentSum 用于记录当前的累加和（前缀和）
         int currentSum = 0;
+        // 遍历数组中的每一个元素
         for (int num : nums) {
             // 更新当前前缀和
             currentSum += num;
             // 如果 (currentSum - k) 在 map 中存在，说明找到了满足条件的子数组
+            // 将对应的前缀和出现次数累加到 count 中
             if (map.containsKey(currentSum - k)) {
                 count += map.get(currentSum - k);
             }
             // 将当前前缀和存入 map，次数 +1
+            // 如果 map 中已存在该前缀和，则在其原有次数基础上加 1
             map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
         }
+        // 返回满足条件的子数组总个数
         return count;
     }
 }
